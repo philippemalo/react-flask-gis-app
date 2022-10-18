b3a8051a7ca561023416cf3c870eaff105a7b4aa
Philippe Malo <philippe.malo21@gmail.com>
2022-10-18 12:35:51 -0400

    frontend project creation

--- src/UserProjects.tsx
+++ src/UserProjects.tsx
@@ -1,8 +1,18 @@
 import { gql, useApolloClient } from "@apollo/client";
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Modal,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useContext, useState } from "react";
 import { UserContext } from "./App";
 import { UserProjectsContainer } from "./styles/UserProjectsContainer.css";
+import AddIcon from "@mui/icons-material/Add";
 
 type Project = {
   id: number;
@@ -12,6 +22,8 @@ type Project = {
 export const UserProjects = () => {
   const [projects, setProjects] = useState([]);
   const client = useApolloClient();
+  const [showCreateProjectModal, setShowCreateProjectModal] = useState(false);
+  const [newProjectName, setNewProjectName] = useState("");
 
   const connectedUser = useContext(UserContext);
 
@@ -32,7 +44,45 @@ export const UserProjects = () => {
     })
     .then((res) => setProjects(res.data.userProjects.projects));
 
-  // Fetch user projects
+  const modalStyle = {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    bgcolor: "background.paper",
+    border: "1px solid #000",
+    boxShadow: 24,
+    p: 3,
+    gap: "10px",
+  };
+
+  const handleCreateProject = () => {
+    client
+      .mutate({
+        mutation: gql`
+          mutation createProject {
+            createProject(projectName: "${newProjectName}", userId: "${connectedUser.user.id}") {
+              success
+              errors
+              project {
+                id
+                name
+              }
+            }
+          }
+        `,
+      })
+      .then((res) => {
+        setShowCreateProjectModal(false);
+        console.log(res);
+      });
+  };
+
+  const handleCloseModal = () => {
+    setShowCreateProjectModal(false);
+  };
 
   return (
     <UserContext.Consumer>
@@ -41,23 +91,85 @@ export const UserProjects = () => {
           window.location.replace("/login");
         } else {
           return (
-            <UserProjectsContainer>
-              {projects.map((project: Project) => (
-                <Card key={project.id} sx={{ maxWidth: 345 }}>
-                  <CardMedia
-                    component="img"
-                    height="140"
-                    image="https://images.landscapingnetwork.com/pictures/images/900x705Max/site_8/oxford-college-of-garden-design_3126.jpg"
-                    alt={project.name}
+            <>
+              <Modal
+                open={showCreateProjectModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+              >
+                <Box sx={modalStyle}>
+                  <Typography
+                    id="modal-modal-title"
+                    variant="h6"
+                    component="h2"
+                  >
+                    Choose a name for your new project
+                  </Typography>
+                  <TextField
+                    id="newproject"
+                    label="Project name"
+                    variant="standard"
+                    onChange={(e) => setNewProjectName(e.currentTarget.value)}
                   />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {project.name}
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                    }}
+                  >
+                    <Button
+                      onClick={handleCreateProject}
+                      disabled={!newProjectName}
+                    >
+                      Create
+                    </Button>
+                    <Button onClick={() => setShowCreateProjectModal(false)}>
+                      Cancel
+                    </Button>
+                  </div>
+                </Box>
+              </Modal>
+              <UserProjectsContainer>
+                <Card
+                  key={"new project"}
+                  sx={{ width: 250, height: 200 }}
+                  onClick={() => setShowCreateProjectModal(true)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <CardContent
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "100%",
+                      padding: 0,
+                    }}
+                  >
+                    <AddIcon fontSize="large" color="primary" />
+                    <Typography variant="h5" component="div">
+                      {"New project"}
                     </Typography>
                   </CardContent>
                 </Card>
-              ))}
-            </UserProjectsContainer>
+                {projects.map((project: Project) => (
+                  <Card key={project.id} sx={{ width: 250, height: 200 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="https://images.landscapingnetwork.com/pictures/images/900x705Max/site_8/oxford-college-of-garden-design_3126.jpg"
+                      alt={project.name}
+                    />
+                    <CardContent>
+                      <Typography variant="h5" component="div">
+                        {project.name}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                ))}
+              </UserProjectsContainer>
+            </>
           );
         }
       }}
